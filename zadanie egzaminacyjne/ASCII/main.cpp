79cddbfef6c13263f7c7f7ee02bb1ba5aabec9e7
Aleksander Okoń <olerandox@gmail.com>
2019-10-15 09:36:13 +0200

    ASCII

---
+++ zadanie egzaminacyjne/ASCII/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+char a;
+int b;
+
+int main()
+{
+    cout << "podaj znak: ";
+    cin >> a;
+    b=a ;
+    cout << "twoj znak w ASCII to: " << b << endl;
+    return 0;
+
+}
