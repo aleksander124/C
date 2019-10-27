931ec435a117f9d3a8079d4370b20b9c02132c48
Aleksander Okoń <aleksander.okon@github.com>
2019-10-27 13:52:34 +0100

    testy

---
+++ zadanie egzaminacyjne/sprawdzian_1/test_ASCII.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+char znak;
+int liczba;
+
+int main(){
+  cout<<"Witaj!"<<'\n'
+  <<"Podaj dowolna litere: ";
+  cin >> znak ;
+  liczba = znak ;
+  cout << '\n' << '\n';
+
+  if(liczba > 64 && liczba < 91){
+    cout << "Podana przez Ciebie litera jest duza\n";
+    if (znak== 'A' || znak== 'E' || znak== 'I' || znak== 'O' || znak== 'U'){
+      cout << "Podana przez Ciebie litera > " << znak << " < Jest samogloska\n" ;
+    }
+    else{
+      cout << "Podana przez Ciebie litera > "<< znak << " < Nie jest samogloska\n" ;
+    }
+  }
+  else if(znak > 60 && znak < 123){
+    cout << "Podana przez Ciebie litera jest mala\n";
+    if (znak== 'a' || znak== 'e' || znak== 'i' || znak== 'o' || znak== 'u'){
+      cout << "Podana przez Ciebie litera > " << znak << " < Jest samogloska\n" ;
+    }
+    else{
+      cout << "Podana przez Ciebie litera > "<< znak << " < Nie jest samogloska\n" ;
+    }
+  }
+  else{
+    cout << "Blad. > " << znak << " < To nie jest litera";
+  }
--- zadanie egzaminacyjne/sprawdzian_1/test_if.cpp
+++ zadanie egzaminacyjne/sprawdzian_1/test_if.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 char znak;
+int liczba;
 
 int main(){
   cout<<"Witaj!"<<'\n'
-  <<"Podaj dowolna samogloske"<<'\n'
-  <<"Pamietej o wpisaniu jej duza litera: ";
+  <<"Podaj dowolna litere\n"
+  <<"Pamietaj o tym zeby byla duza litera: ";
   cin >> znak ;
   cout << '\n' << '\n';
 
-  if (znak== 'A' || znak== 'E' || znak=='I' || znak=='O' || znak=='U') {
+ if (znak== 'A' || znak== 'E' || znak=='I' || znak=='O' || znak=='U') {
     cout << "Podana przez Ciebie litera > " << znak << " < Jest samogloska\n" ;
   }
   else{
     cout << "Podana przez Ciebie litera > "<< znak << " < Nie jest samogloska\n" ;
   }
   return 0;
+
 }
